f0aa1ee1895522a49838b65500ab34b1f4775f38
Lidor Sanker <57188775+lidor7776@users.noreply.github.com>
2023-12-13 15:19:50 +0200

    adding middlewares

--- src/app.js
+++ src/app.js
@@ -1,9 +1,53 @@
 import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import helmet from "helmet";
+import MongoSanitize from "express-mongo-sanitize";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import fileUpload from "express-fileupload";
+import cors from "cors"
 
+//dotEnv config
+dotenv.config();
+
+//create express app
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello from server");
+//Morgan
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
+
+//Helmet
+app.use(helmet());
+
+//Parse json request url
+app.use(express.json());
+//Parse json request body
+app.use(express.urlencoded({ extended: true }));
+
+//sanitize request data
+app.use(MongoSanitize());
+
+//enable cookie parser
+app.use(cookieParser());
+
+//gzip compression
+app.use(compression());
+
+//file upload
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
+//cors
+app.use(cors());
+
+app.post("/", (req, res) => {
+  res.send(req.body);
 });
 
-export default  app;
+export default app;

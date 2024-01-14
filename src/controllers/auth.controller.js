eaf33d585266fa5239cd2c2c1be5fcf67d107c1d
Lidor Sanker <57188775+lidor7776@users.noreply.github.com>
2024-01-14 13:46:55 +0200

    log out function

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,31 +1,107 @@
-export const register = async(req,res,next)=>{
-    try{
-        res.send(req.body)
-    }   catch(error){
-        next(error);
-    }
+import { createUser, signUser } from "../services/auth.service.js";
+import { generateToken } from "../services/token.service.js";
+
+export const register = async (req, res, next) => {
+  try {
+    const { name, email, picture, status, password } = req.body;
+    const newUser = await createUser({
+      name,
+      email,
+      picture,
+      status,
+      password,
+    });
+    const access_token = await generateToken(
+      { userId: newUser._id },
+      "1d",
+      process.env.ACCESS_TOKEN_SECRET
+    );
+
+    const refresh_token = await generateToken(
+      { userId: newUser._id },
+      "30d",
+      process.env.REFRESH_TOKEN_SECRET
+    );
+
+    res.cookie("refreshtoken", refresh_token, {
+      httpOnly: true,
+      path: "/api/v1/auth/refreshtoken",
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    });
+
+    console.table({ access_token, refresh_token });
+   
+    res.json({
+      massage: "register success.",
+      access_token,
+      user: {
+        _id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+        picture: newUser.picture,
+        status: newUser.status,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const login = async(req,res,next)=>{
-    try{
+export const login = async (req, res, next) => {
+  try {
+    const {email,password}=req.body;
+    const user=await signUser(email,password);
+    
+    const access_token = await generateToken(
+      { userId: user._id },
+      "1d",
+      process.env.ACCESS_TOKEN_SECRET
+    );
 
-    }   catch(error){
-        next(error);
-    }
+    const refresh_token = await generateToken(
+      { userId: user._id },
+      "30d",
+      process.env.REFRESH_TOKEN_SECRET
+    );
+
+    res.cookie("refreshtoken", refresh_token, {
+      httpOnly: true,
+      path: "/api/v1/auth/refreshtoken",
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    });
+
+    console.table({ access_token, refresh_token });
+   
+    res.json({
+      massage: "register success.",
+      access_token,
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        picture: user.picture,
+        status: user.status,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const logout = async(req,res,next)=>{
-    try{
-
-    }   catch(error){
-        next(error);
-    }
+export const logout = async (req, res, next) => {
+  try {
+    res.clearCookie('refreshtoken',{path:'/api/v1/auth/refreshtoken'});
+    res.json({
+      massage:'logged out!!',
+    })
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const refreshToken = async(req,res,next)=>{
-    try{
-
-    }   catch(error){
-        next(error);
-    }
+export const refreshToken = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
 };
